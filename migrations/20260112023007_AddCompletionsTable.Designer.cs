8e298bc15a56f124ffe1e38adde5849814b69ba3
yuhong@jfetek.com <yuhong@jfetek.com>
2026-01-12 10:35:21 +0800

    Add EF migration for Completions table

--- migrations/20260112023007_AddCompletionsTable.Designer.cs
+++ migrations/20260112023007_AddCompletionsTable.Designer.cs
@@ -2,6 +2,7 @@
 using ARCompletions.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ARCompletions.migrations
 {
     [DbContext(typeof(ARCompletionsContext))]
-    partial class ARCompletionsContextModelSnapshot : ModelSnapshot
+    [Migration("20260112023007_AddCompletionsTable")]
+    partial class AddCompletionsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
---
+++ migrations/20260112023007_AddCompletionsTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ARCompletions.migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompletionsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
