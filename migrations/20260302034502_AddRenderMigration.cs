9cfd7cfa89da3bd734a4ab8e4aea0d56bb586c80
yuhong@jfetek.com <yuhong@jfetek.com>
2026-03-02 11:49:19 +0800

    Add EF migration and enable RUN_MIGRATIONS in render.yaml

---
+++ migrations/20260302034502_AddRenderMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ARCompletions.migrations
+{
+    /// <inheritdoc />
+    public partial class AddRenderMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
