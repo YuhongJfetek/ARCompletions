c94e413786ce07e865e8bb174f171f2a80d3fe8d
yuhong@jfetek.com <yuhong@jfetek.com>
2026-03-09 14:35:10 +0800

    Add SyncModel migration

--- migrations/20260309063510_SyncModel.Designer.cs
+++ migrations/20260309063510_SyncModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ARCompletions.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ARCompletions.migrations
 {
     [DbContext(typeof(ARCompletionsContext))]
-    partial class ARCompletionsContextModelSnapshot : ModelSnapshot
+    [Migration("20260309063510_SyncModel")]
+    partial class SyncModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
